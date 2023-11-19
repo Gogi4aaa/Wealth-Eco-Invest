f2f4c4a0bb3239af4c4992796ed331a208ac0cab
Gogi4aaa <georgi-07@abv.bg>
2023-11-19 23:46:37 +0200

    add username for login and register

---
+++ Wealth-Eco-Invest.Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Wealth_Eco_Invest.Data
+{
+	using Microsoft.AspNetCore.Identity;
+
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser,IdentityRole<Guid>, Guid>
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}
---
+++ Wealth-Eco-Invest.Data/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿namespace Wealth_Eco_Invest.Data
+{
+	using System.ComponentModel.DataAnnotations;
+	using Microsoft.AspNetCore.Identity;
+	
+	using static Common.ValidationConstants.User;
+
+	public class ApplicationUser : IdentityUser<Guid>
+	{
+		public ApplicationUser()
+		{
+			this.Id = Guid.NewGuid();
+        }
+
+	}
+}
