bb418983a8f0586e3e4f24e9f4212e0a34e1e4a1
Gogi4aaa <georgi-07@abv.bg>
2024-02-11 02:53:45 +0200

    add design changes

--- Wealth-Eco-Invest.Common/ValidationConstants.cs
+++ Wealth-Eco-Invest.Common/ValidationConstants.cs
@@ -12,8 +12,8 @@
         {
             public const int TitleMaxLength = 30;
             public const int TitleMinLength = 5;
-            public const int DescriptionMaxLength = 90;
-            public const int DescriptionMinLength = 5;
+            public const int DescriptionMaxLength = 200;
+            public const int DescriptionMinLength = 35;
             public const int ImageUrlMaxLength = 1000;
             public const int PriceMinLength = 1;
             public const int PriceMaxLength = 100_000;
